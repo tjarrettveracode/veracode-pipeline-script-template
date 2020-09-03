9cefd1e3365f64d97b15a99a97436f7490760111
Tim Jarrett <tjarrett@veracode.com>
2020-09-03 14:00:33 -0400

    move to veracode-api-py

--- vcpipelinescript.py
+++ vcpipelinescript.py
@@ -6,27 +6,27 @@ from lxml import etree
 from io import BytesIO
 import json
 import datetime
-from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 
-from helpers import api
+from veracode_api_py import VeracodeAPI as vapi
+
 from helpers import constants
 
 def creds_expire_days_warning():
-    creds = api.VeracodeAPI().get_creds()
+    creds = vapi().get_creds()
     exp = datetime.datetime.strptime(creds['expiration_ts'], "%Y-%m-%dT%H:%M:%S.%f%z")
     delta = exp - datetime.datetime.now().astimezone() #we get a datetime with timezone...
     if (delta.days < 7):
         print('These API credentials expire ', creds['expiration_ts'])
 
 def getapplication(appid):
-    appinfo = api.VeracodeAPI().get_app(legacy_id=appid)
+    appinfo = vapi().get_app(legacy_id=appid)
     appinfo = appinfo['_embedded']['applications'][0]
     return appinfo
 
 def getpolicy(appinfo):
     policy_guid = appinfo['profile']['policies'][0]['guid']
     # get policy using API
-    return api.VeracodeAPI().get_policy(policy_guid)
+    return vapi().get_policy(policy_guid)
 
 def getstaticscan(appinfo):
     scan_url = None
@@ -54,7 +54,7 @@ def parse_and_remove_xml_namespaces(xml_string):
     return it.root
 
 def getdetailedreport(build_id):
-    detailedreport = api.VeracodeAPI().get_detailed_report(build_id)
+    detailedreport = vapi().get_detailed_report(build_id)
     detailedreport_root = parse_and_remove_xml_namespaces(detailedreport) #manipulating with ns is a pain
     return detailedreport_root
 
